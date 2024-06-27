04c7cf63bce4bcc2c87b8857ccf0cbf7bfc4f5b0
godwinsodogandji <jeansodogandji@gmail.com>
2024-06-27 22:08:17 +0100

    Page d'accueil ok

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+ <router-view></router-view>
+</template>
+
+<script setup>
+import AcceuilComponent from "@/Components/AcceuilComponent.vue";
+
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory } from "vue-router";
+import AcceuilComponent from "../Components/AcceuilComponent.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-  ]
-})
+    {
+      path: "/",
+      name: "Acceuil",
+      component: AcceuilComponent,
+    },
+   
+  ],
+});
 
-export default router
+export default router;
