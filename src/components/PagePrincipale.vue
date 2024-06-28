9f36a563d2a94aba6deda9551ecaeca7d2041442
Hospice Ahouandjinou <hospicesegnongiovanni@gmail.com>
2024-06-28 13:26:51 +0200

    Modif

--- src/components/PagePrincipale.vue
+++ src/components/PagePrincipale.vue
@@ -67,6 +67,18 @@
 
 <script setup>
 import timeComponent from "@/components/timeComponent.vue";
+
+
+const number = [
+    {digit:'3', value:3},
+    {digit:'5', value:5},
+    {digit:'10', value:10},
+    {digit:'15', value:15},
+    {digit:'20', value:20},
+    {digit:'💪30', value:30},
+    {digit:'😂100', value:100},
+    ]
+const taked = ref(3);
 </script>
 
 <style scoped>
