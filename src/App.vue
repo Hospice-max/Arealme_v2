7ffcff71426817d69ec6bb53b60b8c3bb2dd559f
ZANNOUVictorin <giornohamoudath@gmail.com>
2024-06-28 10:25:25 +0200

    ajout de l'heure

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import timeComponent from "./Components/timeComponent.vue"
+</script>
+<template>
+    
+    <timeComponent /> 
+  </template>
+
+<style>
+
+</style> 
---
+++ src/Components/timeComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <span>
+    {{z -1 }}:
+  </span>
+  <span>
+    {{ a }}:
+  </span>
+  <span>
+    {{y}}
+  </span>
+  
+  
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let now = new Date();
+let heure = ref(new Date( ).getSeconds());
+let y = ref(0);
+let z = ref(0);
+let a = ref(0);
+
+function name() {
+
+   y.value = new Date().getSeconds();
+   a.value=new Date().getMinutes();
+   z.value=new Date().getHours();
+     return y.value ,a.value,z.value;
+}
+
+setInterval (name, 1000);
+
+
+</script>
+
+<style>
+</style>
