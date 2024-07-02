6ca792c4076351d0e6b527f49410c3a605a173ce
Hospice Ahouandjinou <hospicesegnongiovanni@gmail.com>
2024-07-02 10:38:16 +0200

    En cours

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
 import AcceuilComponent from "@/views/AcceuilPage.vue";
-
-import AcceuilComponent from "@/views/PagePrincipale.vue";
 import TableauScore from "@/components/TableauScore.vue";
 </script>
 
 <template>
-</template>
-
-
 <div>
   <RouterLink to="/"></RouterLink>
   <RouterView/>
